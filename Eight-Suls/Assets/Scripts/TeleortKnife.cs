2c2ea1a24366f247bf81813f632bb26e991a5324
CollinWilkerson <wilkerc2@my.erau.edu>
2026-02-16 12:56:56 -0700

    create thrown teleport object

---
+++ Eight-Suls/Assets/Scripts/TeleortKnife.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TeleortKnife : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    private bool isPrimed = false;
+
+
+    public void PrimeOnThrow()
+    {
+        isPrimed = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isPrimed)
+        {
+            player.transform.position = transform.position;
+        }
+    }
+}
