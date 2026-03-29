14b4b78b0239497c0313afe7b72720dfe1adbae5
CollinWilkerson <wilkerc2@my.erau.edu>
2026-03-29 14:29:33 -0700

    Set up enemy nav mesh

--- Eight-Suls/Assets/Scripts/EnemyBehavior.cs
+++ Eight-Suls/Assets/Scripts/EnemyBehavior.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
-
+    NavMeshAgent navAgent;
     [SerializeField] private int health = 3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GetNavMeshAgent();
+    }
+
+    private void GetNavMeshAgent()
+    {
+        if (!gameObject.GetComponent<NavMeshAgent>())
+        {
+            Debug.LogError("NO NAV MESH AGENT ON ENEMY: " + gameObject.name);
+            return;
+        }
+        navAgent = gameObject.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
