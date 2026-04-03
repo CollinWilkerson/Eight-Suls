5a429468ee179d9ec52a7f623aaf7cd86e84aba9
CollinWilkerson <156472381+CollinWilkerson@users.noreply.github.com>
2026-04-02 17:29:47 -0700

    set up scene for sword respawn

--- Eight-Suls/Assets/Scripts/SwordBehavior.cs
+++ Eight-Suls/Assets/Scripts/SwordBehavior.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 //I feel like this is missing some nuance
@@ -6,14 +7,22 @@ using UnityEngine;
 public class SwordBehavior : MonoBehaviour
 {
     [SerializeField] string damageType;
+    [SerializeField] float respawnTime = 1f;
 
     private Vector3 hitLocation;
     private bool hitObject = false;
     private bool delayFrame = false;
     private static DamageDisplayBehavior damageDisplay;
+    private bool attackStance = true;
+    private Rigidbody rb;
+    private Vector3 startPosition;
+    private Quaternion startRotation;
 
     private void Start()
     {
+        rb = gameObject.GetComponent<Rigidbody>();
+        startPosition = transform.position;
+        startRotation = transform.rotation;
         if(damageDisplay == null)
         {
             damageDisplay = FindAnyObjectByType<DamageDisplayBehavior>();
@@ -39,7 +48,7 @@ public class SwordBehavior : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         //Debug.Log("Collision with" + collision.gameObject.name);
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy") && attackStance)
         {
             if(collision.gameObject.GetComponent<EnemyBehavior>() != null)
             {
@@ -52,5 +61,42 @@ public class SwordBehavior : MonoBehaviour
                 hitObject = true;
             }
         }
+        if (collision.gameObject.CompareTag("EnemyWeapon"))
+        {
+            if (attackStance)
+            {
+                DestroySword();
+            }
+        }
+    }
+
+    public void ActivateOnPickup()
+    {
+        rb.isKinematic = false;
+    }
+
+    public void EnterGuardStance()
+    {
+        attackStance = false;
+    }
+
+    public void ExitGuardStance()
+    {
+        attackStance = true;
+    }
+
+    public void DestroySword()
+    {
+        rb.isKinematic = true;
+        StartCoroutine(RespawnSword());
+        gameObject.SetActive(false); //replace this with some animation
+    }
+
+    public IEnumerator RespawnSword()
+    {
+        yield return new WaitForSeconds(respawnTime);
+        transform.position = startPosition;
+        transform.rotation = startRotation;
+        gameObject.SetActive(true);
     }
 }
